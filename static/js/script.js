5f92ad1c9482e14d189afb52b71cc499708e3922
guibraz <secondbraz@gmail.com>
2025-02-23 16:29:34 -0300

    no ai starting

--- static/js/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("generateNameButton").addEventListener("click", function() {
-    fetch("/random-name")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("randomName").innerText = data;
-        })
-        .catch(error => console.error('Error:', error));
-});
